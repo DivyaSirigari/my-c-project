b25ca0b23915f45369c2c5763b8215a03342bc44
DivyaSirigari <106544947+DivyaSirigari@users.noreply.github.com>
2022-05-31 22:41:06 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 void main(){
     char name[10];
-
+    int age;
     printf("what is your name");
     scanf("%s",&name);
     printf("hello, %s", name);
+    printf("what is your age");
+    scanf("%d",&age);
+    printf("your age %d", age);
 }
